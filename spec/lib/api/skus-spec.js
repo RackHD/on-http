66c355df7fe87443cc2c6af9631c684d790d61e5
Jason Gardino <jason.gardino@renasar.com>
2015-02-02 12:59:06 -0800

    adding sku apis

---
+++ spec/lib/api/skus-spec.js
@@ -0,0 +1,8 @@
+// Copyright 2014-2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    it('needs specs');
+});
