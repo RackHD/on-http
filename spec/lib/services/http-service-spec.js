347b40f27d517cfb7bbce84995d5c68980e142fe
Jason Gardino <jason.gardino@renasar.com>
2015-02-10 14:29:24 -0800

    Adding tests for HTTP service and API router

--- spec/lib/services/http-service-spec.js
+++ spec/lib/services/http-service-spec.js
@@ -3,6 +3,28 @@
 
 'use strict';
 
-describe(require('path').basename(__filename), function () {
-    it('needs specs');
+describe('Http.Server', function () {
+    var sockJs = {};
+    var app = require('express')();
+    var server;
+
+    before(function() {
+        // use helper.setupInjector because we don't want to start core services
+        helper.setupInjector(_.flatten([
+            helper.require('/lib/services/http-service.js'),
+            dihelper.simpleWrapper(sockJs, 'SockJS.Server'),
+            dihelper.simpleWrapper(app, 'express-app', undefined, __dirname),
+        ]));
+
+        sockJs.listen = sinon.stub();
+        server = helper.injector.get('Http.Server');
+    });
+
+    it('should listen on HTTP port and start the websocket listener', function () {
+        // can't use port 80 because it requires setuid root
+        server.listen(8089);
+        expect(sockJs.listen).to.have.been.calledOnce;
+        server.close();
+    });
+
 });
