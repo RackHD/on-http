d73f28ccd0539d81c0fc05b1fd431eff8f5519d3
Jason Gardino <jason.gardino@renasar.com>
2015-02-10 16:36:06 -0800

    adding helper for HTTP api tests

--- spec/helper.js
+++ spec/helper.js
@@ -6,3 +6,26 @@ var di = require('di');
 global.core = require('renasar-core')(di);
 global.dihelper = core.helper;
 
+helper.startServer = function (overrides) {
+
+    helper.setupInjector(_.flatten([
+            require('renasar-tasks').injectables,
+            dihelper.simpleWrapper(require('renasar-mq'), 'MQ'),
+            dihelper.simpleWrapper(require('express')(), 'express-app'),
+            dihelper.simpleWrapper({
+                publishLog: sinon.stub().returns(Q.resolve())
+            }, 'Protocol.Logging'),
+            dihelper.simpleWrapper({
+                lookupIpLease: sinon.stub().returns(Q.resolve('00:00:00:00:00:00'))
+            }, 'Protocol.Dhcp'),
+            helper.requireGlob('/lib/**/*.js'),
+            helper.require('/app.js')
+    ].concat(overrides || [])));
+    helper.setupTestConfig();
+    helper.injector.get('Services.Configuration').set('httpport', 8089);
+    return helper.injector.get('Http').start();
+};
+
+helper.stopServer = function () {
+    return helper.injector.get('Http').stop();
+};
