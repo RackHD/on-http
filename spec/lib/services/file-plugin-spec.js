f83ad006c1300c4d0a2e70c08dd447919e1ea039
Torrent Glenn <tglenn@bennington.edu>
2015-02-12 14:27:37 -0800

    added disk plugin tests

---
+++ spec/lib/services/file-plugin-spec.js
@@ -0,0 +1,135 @@
+"use strict";
+
+require('../../helper.js');
+
+var EventEmitter = require('events').EventEmitter;
+
+
+describe("fileService disk backend", function() {
+    var BackendFS,
+        backend,
+        config,
+        fakeFile,
+        q;
+
+    beforeEach(function(){
+        helper.setupInjector(
+            _.flatten([
+                helper.require('/lib/services/files/file-plugin')
+            ])
+        );
+
+
+        config = {
+            root: '.'
+        };
+
+        fakeFile = {
+            filename: 'fauxFile_uuidGoesHere.txt',
+            basename: 'fauxFile.txt',
+            uuid: 'IamUniversallyUnique',
+            md5: 'I am a hash',
+            sha: 'I am another hash'
+        };
+
+        q = helper.injector.get('Q');
+        BackendFS = helper.injector.get('Files.FS');
+        backend = new BackendFS(config);
+    });
+
+    it("should expose get/put/list/delete methods", function(){
+
+        backend.should.have.property('get')
+        .that.is.a('function').with.length(1);
+
+        backend.should.have.property('put')
+        .that.is.a('function').with.length(1);
+
+        backend.should.have.property('getMeta')
+        .that.is.a('function').with.length(1);
+
+        backend.should.have.property('delete')
+        .that.is.a('function').with.length(1);
+
+        backend.should.have.property('list')
+        .that.is.a('function').with.length(1);
+
+    });
+
+    describe('get', function() {
+        var readStreamStub,
+            findStub,
+            existsStub;
+
+        beforeEach(function() {
+            readStreamStub = sinon.stub(backend, 'createReadStream'),
+            findStub = sinon.stub(backend, 'findOneEntry'),
+            existsStub = sinon.stub(backend, 'exists');
+        });
+
+        it("should resolve a promise on successful get", function() {
+
+            findStub.returns(q.resolve(fakeFile));
+            existsStub.returns(q.resolve(fakeFile));
+            readStreamStub.returns('this would be a stream');
+
+            var rdStreamPromise = backend.get('a uuid');
+
+            return rdStreamPromise.should.eventually.equal('this would be a stream');
+        });
+
+        it("should fail on get if the file is not found", function() {
+
+            findStub.returns(q.reject('file not found'));
+
+            var rdStreamPromise = backend.get('a uuid');
+
+            return rdStreamPromise.should.be.rejectedWith('file not found');
+        });
+    });
+
+    it("should resolve a promise for a writestream on put", function() {
+        var writeStreamStub = sinon.stub(backend, 'createWriteStream'),
+            fakeStream = new EventEmitter();
+
+        sinon.stub(backend, 'createEntry');
+        writeStreamStub.returns(fakeStream);
+
+        var streamObjPromise = backend.put('file.txt');
+
+        fakeStream.emit('metadata', {md5: 'an md5', sha256: 'a sha'});
+
+        return streamObjPromise.should.eventually.have.property('stream')
+        .that.deep.equals(fakeStream);
+    });
+
+
+    it("should return a list of available files", function() {
+        var findStub = sinon.stub(backend, 'findEntries');
+        fakeFile.toJSON = sinon.stub();
+
+        fakeFile.toJSON.returns(fakeFile);
+        findStub.returns(q.resolve([fakeFile]));
+
+        var arrayPromise = backend.list();
+
+        return arrayPromise.should.eventually.deep.equal([fakeFile]);
+    });
+
+    it("should fail on delete if the file is not found", function() {
+        var findStub = sinon.stub(backend, 'findOneEntry');
+
+        sinon.stub(backend, 'unlink');
+        sinon.stub(backend, 'destroyEntry');
+
+
+
+        findStub.returns(q.resolve('file not found'));
+
+        var deletionPromise = backend.delete('a uuid');
+
+        return deletionPromise.should.eventually
+        .be.rejectedWith('file not found');
+    });
+
+});
--- spec/lib/services/file-service-spec.js
+++ spec/lib/services/file-service-spec.js
@@ -1,9 +1,37 @@
 'use strict';
 
 require('../../helper');
-var _ = require('lodash'),
-    EventEmitter = require('events').EventEmitter;
+var EventEmitter = require('events').EventEmitter,
+    stream = require('stream');
 
+
+var MockBack = function (){
+    var self = this;
+
+    this.readString = "Incredibly legible string";
+    this.writeString = "";
+
+    this.mockRdStream = new stream.Readable();
+    this.mockWrStream = new stream.Writable();
+
+    this.mockRdStream._read = function() {
+        this.push(self.readableString);
+        this.push(null);
+    };
+
+    this.mockWrStream._write = function(chunk, encoding, done) {
+        self.writableString += chunk.toString();
+        done();
+    };
+
+    this.get = sinon.stub();
+    this.put = sinon.stub();
+    this.delete = sinon.stub();
+    this.list = sinon.stub();
+    this.getMeta = sinon.stub();
+
+    return this;
+   };
 
 
 
@@ -16,11 +44,11 @@ describe("File Service", function() {
         config;
 
     before(function() {
-        injector = helper.baseInjector.createChild(
+        helper.setupInjector(
             _.flatten([
                helper.require('/lib/services/file-service'),
                 helper.require('/lib/services/files/file-plugin'),
-                helper.require('/lib/services/files/mockfile-plugin')
+                dihelper.simpleWrapper(MockBack, 'Files.Mock')
                     ])
             );
 
@@ -41,7 +69,7 @@ describe("File Service", function() {
             }
         };
 
-        var  FileService = injector.get('fileService');
+        var  FileService = helper.injector.get('fileService');
         fileService = new FileService();
         fileService.injectorMap.MockFS = 'Files.Mock';
         fileService.start(config);
@@ -57,7 +85,7 @@ describe("File Service", function() {
             .that.is.a('function').with.length(3);
 
         fileService.should.have.property('list')
-            .that.is.a('function').with.length(1);
+            .that.is.a('function').with.length(2);
 
         fileService.should.have.property('verify')
             .that.is.a('function').with.length(2);
@@ -67,15 +95,15 @@ describe("File Service", function() {
     });
 
     before(function() {
-        injector = helper.baseInjector.createChild(
+        injector = helper.setupInjector(
             _.flatten([
                 helper.require('/lib/services/file-service'),
                 helper.require('/lib/services/files/file-plugin'),
-                helper.require('/lib/services/files/mockfile-plugin')
+                dihelper.simpleWrapper(MockBack, 'Files.Mock')
                     ])
             );
 
-        FileService = injector.get('fileService');
+        FileService = helper.injector.get('fileService');
 
     });
 
@@ -108,13 +136,15 @@ describe("File Service", function() {
     });
 
     describe("File Service Methods", function() {
+        var q,
+            mockBack;
 
         beforeEach(function() {
-            injector = helper.baseInjector.createChild(
+            injector = helper.setupInjector(
                 _.flatten([
                     helper.require('/lib/services/file-service'),
                     helper.require('/lib/services/files/file-plugin'),
-                    helper.require('/lib/services/files/mockfile-plugin')
+                    dihelper.simpleWrapper(MockBack, 'Files.Mock')
                         ])
                 );
             config = {
@@ -124,38 +154,63 @@ describe("File Service", function() {
 
                 };
 
-            FileService = injector.get('fileService');
+            q = helper.injector.get('Q');
+            FileService = helper.injector.get('fileService');
             fileService = new FileService();
             fileService.injectorMap.MockFS = 'Files.Mock';
             fileService.start(config);
             fakeStream = new EventEmitter();
+            mockBack = fileService.backEnds.defaultBackend;
         });
 
 
         it("should provide the correct hashes for uploaded files", function() {
             var crypto = require('crypto'),
-                stringToHash = fileService.backEnds.defaultBackend.readableString,
+                stringToHash = fileService.backEnds.defaultBackend.readString,
                 md5Hash = crypto.createHash('md5'),
-                shaHash = crypto.createHash('sha256');
+                shaHash = crypto.createHash('sha256'),
+                emittedHash = {};
 
+
+            fileService.backEnds.defaultBackend.put.returns(q.resolve(
+                {
+                    stream: mockBack.mockWrStream,
+                    id: 'a uuid'
+                }
+            ));
 
             md5Hash.update(stringToHash);
             shaHash.update(stringToHash);
 
-            fileService.put(fakeStream, {filename:'unimportant'});
+            var hashes = {
+                md5: md5Hash.digest('hex'),
+                sha: shaHash.digest('hex')
+            };
 
-            fileService.backEnds.defaultBackend.mockWrStream.on('metadata', function(meta) {
+            mockBack.mockWrStream.on('metadata', function(meta) {
 
-                meta.md5.should.equal(md5Hash.digest('hex'));
-
-                meta.sha256.should.equal(shaHash.digest('hex'));
+                emittedHash.md5 = meta.md5;
+                emittedHash.sha = meta.sha256;
             });
+
+            fileService.put(fakeStream, {filename:'unimportant'})
+            .then(function(streamObj) {
+                mockBack.mockRdStream.pipe(streamObj.stream);
+            })
+            .then(function() {
+                emittedHash.should.deep.equal(hashes);
+            });
+
+
         });
 
 
         it("should return a promise for a readstream on get ", function() {
 
-            return fileService.get({filename: 'fake.txt'})
+            fileService.backEnds.defaultBackend.get
+            .returns(q.resolve(mockBack.mockRdStream));
+
+            return fileService.get({filename: 'unimportant.txt'})
             .should.eventually.deep
             .equal(fileService.backEnds.defaultBackend.mockRdStream);
 
@@ -163,26 +218,31 @@ describe("File Service", function() {
 
         it("should return a rejected promise " +
                 "if the backend doesn't have a requested file", function() {
+            mockBack.get.returns(q.reject('file not found'));
 
             return fileService.get({filename: 'notInDatabase.fake'})
-            .should.be.rejected;
+            .should.be.rejectedWith('file not found');
 
         });
 
         it("should return a rejected promise for attemtps to delete " +
         "a not found file", function() {
+            mockBack.delete.returns(q.reject('file not found'));
 
             return fileService.delete({filename: "notInDatabase.txt"})
-            .should.be.rejected;
+            .should.be.rejectedWith('file not found');
 
         });
 
 
         it("should return a promise for the list " +
         "received from call to backend's list method",function() {
+            var fileList = ['aFile.txt', 'fauxFile.txt', 'falseFile.txt'];
+
+            mockBack.list.returns(q.resolve(fileList));
 
             return fileService.list().should.eventually
-            .deep.equal(fileService.backEnds.defaultBackend.fileList);
+            .deep.equal(fileList);
 
         });
 
