a2da580d8d2b56b8d84e3b91590b259a76cf98b7
Jason Gardino <jason.gardino@renasar.com>
2015-03-02 18:10:49 -0800

    Adding config, obms, templates API tests

--- spec/lib/api/config-spec.js
+++ spec/lib/api/config-spec.js
@@ -3,6 +3,35 @@
 
 'use strict';
 
-describe('/config', function () {
-    it('needs specs');
+describe('Http.Api.Config', function () {
+    before('start HTTP server', function () {
+        this.timeout(5000);
+        return helper.startServer();
+    });
+
+    after('stop HTTP server', function () {
+        return helper.stopServer();
+    });
+
+    it('should return configuration', function () {
+        var configuration = helper.injector.get('Services.Configuration');
+        return helper.request().get('/api/1.1/config')
+            .expect('Content-Type', /^application\/json/)
+            .expect(200)
+            .expect(function (res) {
+                expect(res.body).to.deep.equal(configuration.getAll());
+            });
+    });
+
+    it('should edit configuration', function () {
+        var configuration = helper.injector.get('Services.Configuration');
+        return helper.request().patch('/api/1.1/config')
+            .send({ dummySetting: 'magic' })
+            .expect('Content-Type', /^application\/json/)
+            .expect(200)
+            .expect(function (res) {
+                expect(res.body).to.have.property('dummySetting').that.equals('magic');
+                expect(res.body).to.deep.equal(configuration.getAll());
+            });
+    });
 });
