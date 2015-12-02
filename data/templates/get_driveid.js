9e4858be15e2cbd6badd88f9250369316174c412
Krein.Peng <zuiliang.peng@emc.com>
2015-12-02 14:27:46 +0800

    Add error handling mechanism and update other comments

--- data/templates/get_driveid.js
+++ data/templates/get_driveid.js
@@ -8,19 +8,20 @@ var cmdDriveWwid = 'ls -l /dev/disk/by-id';
 var cmdVdInfo = 'ls -l /dev/disk/by-path';
 var cmdScsiId = 'lsscsi';
 var options = {
-    timeout: 10000
+    timeout: 10000 //10 seconds
 };
 
 /**
  * Parse the Drive WWID output
- * @param {String} data
- * @return
+ * @param {String} idList - stdout for command 'ls -l /dev/disk/by-id'
+ * @return {Object} include device name, Linux and ESXi WWIDs for each disks
  */
 function parseDriveWwid(idList) {
     //idList example
     //lrwxrwxrwx. 1 root root  9 Nov 18 20:30 ata-SATADOM-SV_3SE_20150522AA9992050085 -> ../../sdb
     var lines = idList.split('\n').map(function(line) {
         var split = line.split(/\s+/);
+        //returned string example ata-SATADOM-SV_3SE_20150522AA9992050085->../../sdb
         return [split[8],split[10]].join('->');
     });
     //According to SCSI-3 spec, vendor specified logic unit name string is 60
@@ -38,6 +39,11 @@ function parseDriveWwid(idList) {
         }
     });
 
+    //ESXi SATA WWID should be ('t10.ATA_____' + VendorInfo + Sub + SerialNumber)
+    //VendorInfo + Sub + SerialNumber should be 60 characters.
+    //Sub is made of N(N = 60 - VendorInfo - SerialNumber) underline '_' symbols
+    //ESXi SATA WWID should finally replace remaing dashs '-' with '2D', '3D' ...
+    //Todo: confrim above analysis is correct for all SATA disks.
     var esxiSata = sataLines.map(function(esxiLine) {
         var line = esxiLine[1];
         var headIndex = line.indexOf('-'), snIndex = line.lastIndexOf('_');
@@ -77,8 +83,8 @@ function parseDriveWwid(idList) {
 
 /**
  * Parse Virtual Drive output via by-path output
- * @param {String} data
- * @return
+ * @param {String} pathList - stdout for command 'ls -l /dev/disk/by-path'
+ * @return {Array} include device name, virtual disk name and scsi ID for each disk
  */
 function parseVdInfo(pathList) {
     var lines = pathList.split('\n').map(function(line) {
@@ -98,31 +104,33 @@ function parseVdInfo(pathList) {
     return pciLines.map(function(line) {
         var scsiId = line[1].slice(line[1].lastIndexOf('-') + 1), vdStr;
         if (scsiId.split(':').length === 4){
-		    var scsiIdArray = scsiId.split(':');
+            var scsiIdArray = scsiId.split(':');
             vdStr = ['/c', scsiIdArray[0], '/v', scsiIdArray[2]].join('');
-		}
-	    return [line[0], vdStr, scsiId];
+        }
+        return [line[0], vdStr, scsiId];
     });
 }
 
 /**
-* Parse scsi ID output via lsscsi command
-* @param {String} data
-*/
+ * Parse scsi ID output via lsscsi command
+ * @param {String} lsscsiList - stdout for command 'lsscsi'
+ * @return {Array} include device name and scsi info for each disk
+ */
 function parseScsiInfo(lsscsiList) {
-	var lines = lsscsiList.split('\n');
-	return lines.map(function(line) {
-		if(line){
-		return [line.slice(line.lastIndexOf('/') + 1).replace(' ', ''),
-            line.slice(1, line.indexOf(']'))];
-		}
-	});
+    var lines = lsscsiList.split('\n');
+    return lines.map(function(line) {
+        if(line){
+            return [line.slice(line.lastIndexOf('/') + 1).replace(' ', ''),
+                line.slice(1, line.indexOf(']'))];
+        }
+    });
 }
 
 /**
  * Build the drive mapping table
  * @param {String} wwidData
  * @param {String} vdData
+ * @param {String} scsiData
  * @return 0 if success, otherwise failed.
  */
 function buildDriveMap(wwidData, vdData, scsiData) {
@@ -150,11 +158,10 @@ function buildDriveMap(wwidData, vdData, scsiData) {
         driveIds.push({"scsiId": scsiId, "virtualDisk": vd,
             "esxiWwid": esxiWwid[1], "devName": diskPath});
     });
-    var k=0;
-    linuxWwids.forEach(function(linuxWwid){
+
+    linuxWwids.forEach(function(linuxWwid, k){
         driveIds[k].identifier = k;
         driveIds[k].linuxWwid = linuxWwid[1];
-        k += 1;
     });
     console.log(JSON.stringify(driveIds));
     return 0;
@@ -162,35 +169,41 @@ function buildDriveMap(wwidData, vdData, scsiData) {
 
 function run() {
     var wwidData, vdData, scsiData;
-    exec(cmdDriveWwid, options, function(err0, stdout0) {
-        if (err0) {
-            console.error(err0.toString());
-            process.exit(1);
-        }
-        wwidData = stdout0;
-        exec(cmdVdInfo, options, function(err1, stdout1) {
-            if (err1) {
-                console.error(err1.toString());
+    try {
+        exec(cmdDriveWwid, options, function (err0, stdout0) {
+            if (err0) {
+                console.error(err0.toString());
                 process.exit(1);
             }
-            vdData = stdout1;
-            exec(cmdScsiId, options, function(err2, stdout2) {
-                if (err2) {
-                    console.error(err2.toString());
+            wwidData = stdout0;
+            exec(cmdVdInfo, options, function (err1, stdout1) {
+                if (err1) {
+                    console.error(err1.toString());
                     process.exit(1);
                 }
-                scsiData = stdout2;
-                if (buildDriveMap(wwidData, vdData, scsiData)) {
-                    console.error('build drive map failed, wwidData='+
-                        wwidData + '\nvdData=' + vdData);
-                    process.exit(1);
-                }
-                else {
-                    process.exit(0);
-                }
+                vdData = stdout1;
+                exec(cmdScsiId, options, function (err2, stdout2) {
+                    if (err2) {
+                        console.error(err2.toString());
+                        process.exit(1);
+                    }
+                    scsiData = stdout2;
+                    if (buildDriveMap(wwidData, vdData, scsiData)) {
+                        console.error('build drive map failed, wwidData=' +
+                            wwidData + '\nvdData=' + vdData);
+                        process.exit(1);
+                    }
+                    else {
+                        process.exit(0);
+                    }
+                });
             });
         });
-    });
+    }
+    catch (e) {
+        console.error(e.message);
+        process.exit(1);
+    }
 }
 
 return run();
