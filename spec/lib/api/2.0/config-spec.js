b88d1eec28baf3ada33f861e900fc776f244cbdc
Geoff Reid <geoffrey.reid@emc.com>
2016-01-25 16:11:08 -0500

    Config route for 2.0 API
    Add unit test for 2.0 config route

---
+++ spec/lib/api/2.0/config-spec.js
@@ -0,0 +1,48 @@
+// Copyright 2015, EMC, Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe('Http.Api.Config v2.0', function () {
+    var configuration;
+    before('start HTTP server', function () {
+        this.timeout(5000);
+        return helper.startServer();
+    });
+
+    beforeEach('set up mocks', function () {
+        configuration = helper.injector.get('Services.Configuration');
+        sinon.stub(configuration, 'set').returns(configuration);
+        sinon.stub(configuration, 'getAll').returns({});
+    });
+
+    afterEach('tear down mocks', function () {
+        configuration.set.restore();
+        configuration.getAll.restore();
+    });
+
+    after('stop HTTP server', function () {
+        return helper.stopServer();
+    });
+
+    it('should return configuration', function () {
+        return helper.request().get('/api/2.0/config')
+            .expect('Content-Type', /^application\/json/)
+            .expect(200, {})
+            .expect(function () {
+                expect(configuration.getAll).to.have.been.calledOnce;
+            });
+    });
+
+    it('should edit configuration', function () {
+        return helper.request().patch('/api/2.0/config')
+            .send({ dummySetting: 'magic' })
+            .expect('Content-Type', /^application\/json/)
+            .expect(200)
+            .expect(function () {
+                expect(configuration.set).to.have.been.calledOnce;
+                expect(configuration.set).to.have.been.calledWith('dummySetting', 'magic');
+                expect(configuration.getAll).to.have.been.calledOnce;
+            });
+    });
+});
