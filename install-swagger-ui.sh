98c9b0f0a79bf91d7ef4a7897d231f0e27b991fa
Zack Young <Zachary.Young@emc.com>
2016-02-03 18:06:52 -0800

    Change to RackHD repo and add install

--- install-swagger-ui.sh
+++ install-swagger-ui.sh
@@ -2,7 +2,7 @@
 
 cd ./static
 rm -rf swagger-ui
-curl -L -o swagger-ui.zip https://github.com/zyoung51/swagger-ui/archive/master.zip
+curl -L -o swagger-ui.zip https://github.com/rackhd/swagger-ui/archive/master.zip
 unzip swagger-ui.zip swagger-ui-master/dist* -d swagger-ui
 mv swagger-ui/swagger-ui-master/dist/* swagger-ui
 rm swagger-ui.zip
