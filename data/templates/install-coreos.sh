696b3a2fb616c14f08f015f9f393b0727c259575
Felix Yuan <felix.yuan@emc.com>
2016-02-24 22:46:30 +0800

    add external repo support for CoreOS installation

--- data/templates/install-coreos.sh
+++ data/templates/install-coreos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -O http://<%=server%>:<%=port%>/api/common/templates/pxe-cloud-config.yml
-sudo coreos-install -d <%=installDisk%> -c pxe-cloud-config.yml -b http://<%=server%>:<%=port%>/coreos
+sudo coreos-install -d <%=installDisk%> -c pxe-cloud-config.yml -b <%=repo%>
 sudo reboot
