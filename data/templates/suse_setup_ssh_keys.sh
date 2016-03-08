f0972cdce035f885948343b3df18bca6aa106168
Andrew Hou <Andrew.Hou@emc.com>
2016-03-08 12:19:33 +0000

    Add openSUSE/SLES bootstrap support

--- data/templates/suse_setup_ssh_keys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -m 700 /home/<%=username%>/.ssh 
-mkdir -m 700 /root/.ssh 
-
-curl http://<%=server%>:<%=port%>/api/common/templates/renasar-ansible.pub > /root/.ssh/authorized_keys 
-cp /root/.ssh/authorized_keys /home/<%=username%>/.ssh
-chown -R <%=uid%>:<%=uid%> /home/<%=username%>/.ssh
