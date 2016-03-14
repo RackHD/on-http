7b798d81afabf5de639ad8fcb577423b75685665
John H <john.hogenmiller@emc.com>
2016-03-14 12:10:21 -0400

    build refactor

---
+++ extra/make-sysdeps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This has to be kept in sync with .travis.yml packages to
+# be effective.
+
+echo "Installing system dependencies"
+sudo apt-get update
+sudo apt-get install -y \
+    debhelper \
+    default-jdk \
+    devscripts \
+    dh-make \
+    git \
+    libkrb5-dev \
+    maven \
+    mongodb \
+    nodejs \
+    nodejs-legacy \
+    npm \
+    rabbitmq-server \
+    pbuilder \
+    ubuntu-dev-tools
---
+++ extra/make-testcoveralls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure we're always in the right directory.
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha --report lcovonly -- $(find spec -name '*-spec.js') -R spec --require spec/helper.js
+cat ./coverage/lcov.info | node_modules/.bin/coveralls
