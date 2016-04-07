993b40c8dcaedb5776cb0fc5a14fc13c2bd466c2
Winson Wang <winson.wang@emc.com>
2016-04-07 05:41:46 -0400

    move network interfaces config to a single file

--- data/templates/install-ubuntu/post-install-ubuntu.sh
+++ data/templates/install-ubuntu/post-install-ubuntu.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# create SSH key for root
 <% if (undefined !== rootSshKey) { -%>
 mkdir /root/.ssh
 echo <%=rootSshKey%> > /root/.ssh/authorized_keys
 chown -R root:root /root/.ssh
 <% } -%>
 
+# create users and SSH key for users
 <% users.forEach(function(user) { -%>
 useradd -u <%=user.uid%> -m -p <%=user.encryptedPassword%> <%=user.name%>
     <%_ if (undefined !== user.sshKey) { _%>
@@ -14,39 +16,3 @@ echo <%=user.sshKey%> > /home/<%=user.name%>/.ssh/authorized_keys
 chown -R <%=user.name%>:<%=user.name%> /home/<%=user.name%>/.ssh
     <%_ } _%>
 <% }); -%>
-
-<% if (undefined !== networkDevices) { -%>
-echo "
-    <%_ networkDevices.forEach(function(n) { _%>
-        <%_ for (p in n) { _%>
-            <%_ ip = n[p]; _%>
-            <%_ if (['ipv4', 'ipv6'].indexOf(p) === -1 || undefined == ip) continue; _%>
-            <%_ if (undefined !== ip.vlanId) { _%>
-                <%_ ip.vlanId.forEach(function(vid) { _%>
-auto <%=n.device%>.<%=vid%>
-iface <%=n.device%>.<%=vid%> inet static
-address <%=ip.ipAddr%>
-netmask <%=ip.netmask%>
-gateway <%=ip.gateway%>
-
-                <%_ }); _%>
-            <%_ } else { _%>
-auto <%=n.device%>
-iface <%=n.device%> inet static
-address <%=ip.ipAddr%>
-netmask <%=ip.netmask%>
-gateway <%=ip.gateway%>
-
-            <%_ } _%>
-        <%_ } _%>
-    <%_ }); _%>
-" > /etc/network/interfaces
-<% } -%>
-
-<% if (undefined !== dnsServers) { -%>
-echo "search <%=domain%>" > /etc/resolv.conf
-    <%_ dnsServers.forEach(function(dns) { _%>
-echo "nameserver <%=dns%>" >> /etc/resolv.conf
-    <%_ }); _%>
-chattr +i /etc/resolv.conf
-<% } -%>
