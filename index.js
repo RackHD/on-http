2347039157b3dd562d0242643a3e36de7d5b0181
Poulter, Roland <Roland.Poulter@emc.com>
2016-05-02 11:28:36 -0700

    remove websocket server

--- index.js
+++ index.js
@@ -51,8 +51,6 @@ function onHttpContextFactory(di, directory) {
         prerequisiteInjectables: _.flattenDeep([
             core.workflowInjectables,
             onTasks.injectables,
-            helper.simpleWrapper(ws, 'ws'),
-            helper.simpleWrapper(ws.Server, 'WebSocketServer'),
             helper.requireWrapper('swagger-express-mw', 'swagger', undefined, __dirname)
         ])
     };
