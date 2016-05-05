3b12f9a56d1d933954ad84f8c225baf001b625e1
Kranti Uppala <uppalk1@kickseed.veyder.emc.com>
2016-05-05 12:25:40 -0400

    Updated skus-spec Unit Test to remove dependencies

--- spec/lib/api/2.0/skus-spec.js
+++ spec/lib/api/2.0/skus-spec.js
@@ -3,58 +3,181 @@
 
 'use strict';
 
-describe('Http.Api.Skus', function () {
+describe('Http.Api.Skus.2.0', function() {
+    var configuration;
+    var waterline;
+    var ObmService;
     var workflowApiService;
-    var _;
+    var nodeApiService;
+    var lookupService;
+    var Promise;
+    var Constants;
+    var Errors;
+    var skuApiService;
+
+
     before('start HTTP server', function () {
         this.timeout(5000);
-        workflowApiService = {
-            createAndRunGraph: sinon.stub()
-        };
-        helper.setupInjector([
-            helper.require("/lib/services/sku-pack-service"),
-            dihelper.simpleWrapper(function() { arguments[1](); }, 'rimraf')
-        ]);
         return helper.startServer([
-            dihelper.simpleWrapper(workflowApiService, 'Http.Services.Api.Workflows')
         ]).then(function() {
-            _ = helper.injector.get('_');
+            waterline = helper.injector.get('Services.Waterline');
+            sinon.stub(waterline.nodes);
+            sinon.stub(waterline.skus);
+            workflowApiService = helper.injector.get('Http.Services.Api.Workflows');
+            nodeApiService = helper.injector.get('Http.Services.Api.Nodes');
+            sinon.stub(nodeApiService);
+            skuApiService = helper.injector.get('Http.Services.SkuPack');
+            Promise = helper.injector.get('Promise');
+            Constants = helper.injector.get('Constants');
+            Errors = helper.injector.get('Errors');
         });
+
     });
 
-    beforeEach('reset test DB', function () {
-        return helper.reset();
+    beforeEach('reset stubs', function () {
+        function resetStubs(obj) {
+            _(obj).methods().forEach(function (method) {
+                if (obj[method] && obj[method].reset) {
+                    obj[method].reset();
+                }
+            }).value();
+        }
+        resetStubs(waterline.nodes);
+        resetStubs(waterline.skus);
+        resetStubs(workflowApiService);
+
     });
 
     after('stop HTTP server', function () {
         return helper.stopServer();
     });
 
+    var node = {
+        id: '1234abcd1234abcd1234abcd',
+        name: 'sku test node',
+        type: 'compute',
+        obmSettings: [
+            {
+                service: 'ipmi-obm-service',
+                config: {
+                    host: '1.2.3.4',
+                    user: 'myuser',
+                    password: 'mypass'
+                }
+            }
+        ],
+        autoDiscover: "false",
+        identifiers: [],
+        createdAt: '010101',
+        updatedAt: '010101',
+        tags: [],
+        sku: '0987'
+    };
+
+    var input = {
+        name: 'my test sku',
+        rules: [
+            {
+                path: 'dmi.dmi.base_board.manufacturer',
+                contains: 'Intel'
+            },
+            {
+                path: 'dmi.memory.total',
+                equals: '32946864kB'
+            }
+        ],
+        discoveryGraphName: 'TestGraph.Dummy',
+        discoveryGraphOptions: { test: 1 },
+        id: '0987'
+    };
+
+    beforeEach(function() {
+        sinon.stub(skuApiService, 'getSkus');
+        sinon.stub(skuApiService, 'postSku');
+        sinon.stub(skuApiService, 'getSkusById');
+        sinon.stub(skuApiService, 'upsertSku');
+        sinon.stub(skuApiService, 'patchSku');
+        sinon.stub(skuApiService, 'getNodesSkusById');
+        sinon.stub(skuApiService, 'regenerateSkus');
+        sinon.stub(skuApiService, 'deleteSkuById');
+        sinon.stub(workflowApiService, 'createAndRunGraph');
+    });
+
+    afterEach(function() {
+        skuApiService.getSkus.restore();
+        skuApiService.postSku.restore();
+        skuApiService.getSkusById.restore();
+        skuApiService.upsertSku.restore();
+        skuApiService.patchSku.restore();
+        skuApiService.regenerateSkus.restore();
+        skuApiService.getNodesSkusById.restore();
+        skuApiService.deleteSkuById.restore();
+        workflowApiService.createAndRunGraph.restore();
+    });
+
+
+
+
+
     it('should return an empty array from GET /skus', function () {
+        skuApiService.getSkus.resolves([]);
         return helper.request().get('/api/2.0/skus')
             .expect('Content-Type', /^application\/json/)
             .expect(200, []);
     });
 
     describe('created SKU', function () {
-        var input;
         var sku;
-        var node;
 
-        beforeEach('create node', function () {
-            var waterline = helper.injector.get('Services.Waterline');
-            return waterline.nodes.create({ name: 'sku test node' }).then(function (node_) {
-                node = node_;
-            });
+        it('should create a sku', function(){
+            skuApiService.postSku.resolves(input);
+            return helper.request().post('/api/2.0/skus')
+                .send(input)
+                .expect('Content-Type', /^application\/json/)
+                .expect(201)
+                .then(function (req) {
+                    sku = req.body;
+                    expect(sku).to.have.property('name').that.equals(input.name);
+                    expect(sku).to.have.property('rules').that.deep.equals(input.rules);
+                    expect(sku).to.have.property('discoveryGraphName')
+                        .that.equals(input.discoveryGraphName);
+                    expect(sku).to.have.property('discoveryGraphOptions')
+                        .that.deep.equals(input.discoveryGraphOptions);
+                });
         });
 
-        beforeEach('reset createAndRunGraph stub', function () {
-            workflowApiService.createAndRunGraph.reset();
+        it('should contain the new sku in GET /skus', function () {
+            skuApiService.getSkus.resolves([input]);
+            return helper.request().get('/api/2.0/skus')
+            .expect('Content-Type', /^application\/json/)
+            .expect(200, [sku]);
         });
 
-        beforeEach('POST /skus', function () {
-            input = {
-                name: 'my test sku',
+
+        it('should return the same sku from GET /skus/:id', function () {
+            skuApiService.getSkusById.resolves(input);
+            return helper.request().get('/api/2.0/skus/' + sku.id)
+            .expect('Content-Type', /^application\/json/)
+            .expect(200)
+            .then(function (res) {
+                    var checkSku = res.body;
+                    console.log("Printing checkSku hereeeeeeeee");
+                    console.log(checkSku);
+                    expect(checkSku).to.have.property('name').that.equals(input.name);
+                });
+        });
+
+        it('should 200 reputting the same sku', function() {
+            skuApiService.upsertSku.resolves(input);
+            return helper.request().put('/api/2.0/skus')
+            .send(input)
+            .expect(200);
+        });
+
+        describe('PATCH /skus/:id', function () {
+            var updated;
+            var updated_input = {
+                name: 'updated sku name',
                 rules: [
                     {
                         path: 'dmi.dmi.base_board.manufacturer',
@@ -66,171 +189,60 @@ describe('Http.Api.Skus', function () {
                     }
                 ],
                 discoveryGraphName: 'TestGraph.Dummy',
-                discoveryGraphOptions: { test: 1 }
+                discoveryGraphOptions: {test: 1},
+                id: '0987'
             };
-
-            return helper.request().post('/api/2.0/skus')
-            .send(input)
-            .expect('Content-Type', /^application\/json/)
-            .expect(201)
-            .then(function (req) {
-                sku = req.body;
-            });
-        });
-
-        it('should have the correct name', function () {
-            expect(sku).to.have.property('name').that.equals(input.name);
-        });
-
-        it('should have the correct rules', function () {
-            expect(sku).to.have.property('rules').that.deep.equals(input.rules);
-        });
-
-        it('should have the correct discoveryGraphName', function () {
-            expect(sku).to.have.property('discoveryGraphName')
-            .that.equals(input.discoveryGraphName);
-        });
-
-        it('should have the correct discoveryGraphOptions', function () {
-            expect(sku).to.have.property('discoveryGraphOptions')
-            .that.deep.equals(input.discoveryGraphOptions);
-        });
-
-        it('should have regenerated SKUs', function () {
-            expect(workflowApiService.createAndRunGraph).to.have.been.calledOnce;
-            expect(workflowApiService.createAndRunGraph)
-                .to.have.been.calledWith({
-                    name: 'Graph.GenerateSku',
-                    options: {
-                        defaults: {
-                            nodeId: node.id
-                        }
-                    }
-                });
-        });
-
-        it('should contain the new sku in GET /skus', function () {
-            return helper.request().get('/api/2.0/skus')
-            .expect('Content-Type', /^application\/json/)
-            .expect(200, [sku]);
-        });
-
-        it('should return the same sku from GET /skus/:id', function () {
-            return helper.request().get('/api/2.0/skus/' + sku.id)
-            .expect('Content-Type', /^application\/json/)
-            .expect(200, _.merge({}, sku, { packInfo: { description: null, version: null } }) );
-        });
-
-        it('should 409 reposting the same sku', function() {
-            return helper.request().post('/api/2.0/skus')
-            .send(input)
-            .expect(409);
-        });
-
-        it('should 200 reputting the same sku', function() {
-            return helper.request().put('/api/2.0/skus')
-            .send(input)
-            .expect(200);
-        });
-
-        describe('PATCH /skus/:id', function () {
-            var updated;
 
             beforeEach('reset createAndRunGraph stub', function () {
                 workflowApiService.createAndRunGraph.reset();
             });
 
-            beforeEach('PATCH /skus/:id', function () {
+            it('should Patch updated name to the sku', function () {
                 sku.name = 'updated sku name';
-                return helper.request().patch('/api/2.0/skus/' + sku.id)
-                .send(sku)
-                .expect('Content-Type', /^application\/json/)
-                .expect(200)
-                .then(function (res) {
-                    updated = res.body;
-                });
-            });
-
-            it('should have an updated name', function () {
-                expect(updated).to.have.property('name').that.equals(sku.name);
-            });
-
-            it('should have the same rules', function () {
-                expect(updated).to.have.property('rules').that.deep.equals(sku.rules);
-            });
-
-            it('should have regenerated SKUs', function () {
-                expect(workflowApiService.createAndRunGraph).to.have.been.calledOnce;
-                expect(workflowApiService.createAndRunGraph)
-                    .to.have.been.calledWith({
-                        name: 'Graph.GenerateSku',
-                        options: {
-                            defaults: {
-                                nodeId: node.id
-                            }
-                        }
+                skuApiService.patchSku.resolves(updated_input);
+                return helper.request().patch('/api/2.0/skus/0987')
+                    .send(input)
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .then(function (res) {
+                        updated = res.body;
+                        console.log(updated);
+                        expect(updated).to.have.property('name').that.equals(sku.name);
+                        expect(updated).to.have.property('rules').that.deep.equals(sku.rules);
                     });
             });
+
         });
-
         describe('GET /skus/:id/nodes', function () {
-            beforeEach('assign the SKU to the node', function () {
-                var waterline = helper.injector.get('Services.Waterline');
-                return waterline.nodes.update(node.id, { sku: sku.id });
-            });
-
             it('should contain the node', function () {
+                skuApiService.getNodesSkusById.resolves([node]);
                 return helper.request().get('/api/2.0/skus/' + sku.id + '/nodes')
-                .expect('Content-Type', /^application\/json/)
-                .expect(200)
-                .expect(function (res) {
-                    expect(res.body).to.be.an.instanceof(Array);
-                    expect(res.body).to.have.length(1);
-                    expect(res.body[0]).to.have.property('id').that.equals(node.id);
-                });
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .expect(function (res) {
+                        expect(res.body).to.be.an.instanceof(Array);
+                        expect(res.body).to.have.length(1);
+                        expect(res.body[0]).to.have.property('id').that.equals(node.id);
+                    });
             });
         });
 
         describe('DELETE /skus/:id', function () {
-            beforeEach('reset createAndRunGraph stub', function () {
-                workflowApiService.createAndRunGraph.reset();
-            });
 
             beforeEach('DELETE /skus/:id', function () {
+                skuApiService.deleteSkuById.resolves();
                 return helper.request().delete('/api/2.0/skus/' + sku.id)
-                .expect(200);
-            });
-
-            it('should have regenerated SKUs', function () {
-                expect(workflowApiService.createAndRunGraph).to.have.been.calledOnce;
-                expect(workflowApiService.createAndRunGraph)
-                    .to.have.been.calledWith({
-                        name: 'Graph.GenerateSku',
-                        options: {
-                            defaults: {
-                                nodeId: node.id
-                            }
-                        }
-                    });
-            });
+                    .expect(200)
+            })
 
             it('should 404 with GET /skus/:id ', function () {
-                return helper.request().get('/api/2.0/skus/' + sku.id)
-                .expect(404);
-            });
-
-            it('should 404 with PATCH /skus/:id', function () {
-                return helper.request().patch('/api/2.0/skus/' + sku.id)
-                .send({})
-                .expect(404);
-            });
-
-            it('should 404 with DELETE /skus/:id', function () {
-                return helper.request().delete('/api/2.0/skus/' + sku.id)
-                .expect(404);
+                skuApiService.getSkusById.rejects(new Errors.NotFoundError('Not Found'));
+                return helper.request().get('/api/2.0/skus/0987')
+                    .expect(404)
             });
 
             it('should return an empty array from GET /skus', function () {
+                skuApiService.getSkus.resolves([]);
                 return helper.request().get('/api/2.0/skus')
                 .expect('Content-Type', /^application\/json/)
                 .expect(200, []);
