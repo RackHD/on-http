4a2edb7785ce2f4c620da278e4bb6e8e823d3505
Kranti Uppala <uppalk1@kickseed.veyder.emc.com>
2016-05-10 10:53:38 -0400

    Improve coverage

--- spec/lib/api/2.0/skus-spec.js
+++ spec/lib/api/2.0/skus-spec.js
@@ -4,17 +4,13 @@
 'use strict';
 
 describe('Http.Api.Skus.2.0', function() {
-    var configuration;
     var waterline;
-    var ObmService;
     var workflowApiService;
     var nodeApiService;
-    var lookupService;
     var Promise;
     var Constants;
     var Errors;
     var skuApiService;
-
 
     before('start HTTP server', function () {
         this.timeout(5000);
@@ -100,6 +96,7 @@ describe('Http.Api.Skus.2.0', function() {
         sinon.stub(skuApiService, 'getNodesSkusById');
         sinon.stub(skuApiService, 'regenerateSkus');
         sinon.stub(skuApiService, 'deleteSkuById');
+
         sinon.stub(workflowApiService, 'createAndRunGraph');
     });
 
@@ -112,6 +109,7 @@ describe('Http.Api.Skus.2.0', function() {
         skuApiService.regenerateSkus.restore();
         skuApiService.getNodesSkusById.restore();
         skuApiService.deleteSkuById.restore();
+
         workflowApiService.createAndRunGraph.restore();
     });
 
@@ -119,7 +117,10 @@ describe('Http.Api.Skus.2.0', function() {
         skuApiService.getSkus.resolves([]);
         return helper.request().get('/api/2.0/skus')
             .expect('Content-Type', /^application\/json/)
-            .expect(200, []);
+            .expect(200, [])
+            .then(function (res) {
+                expect(skuApiService.getSkus).to.have.been.called;
+            });
     });
 
     describe('created SKU', function () {
@@ -145,16 +146,20 @@ describe('Http.Api.Skus.2.0', function() {
         it('should contain the new sku in GET /skus', function () {
             skuApiService.getSkus.resolves([input]);
             return helper.request().get('/api/2.0/skus')
-            .expect('Content-Type', /^application\/json/)
-            .expect(200, [sku]);
+                .expect('Content-Type', /^application\/json/)
+                .expect(200, [sku])
+                .then(function (res) {
+                    expect(skuApiService.getSkus).to.have.been.called;
+            });
         });
 
         it('should return the same sku from GET /skus/:id', function () {
             skuApiService.getSkusById.resolves(input);
             return helper.request().get('/api/2.0/skus/' + sku.id)
-            .expect('Content-Type', /^application\/json/)
-            .expect(200)
-            .then(function (res) {
+                .expect('Content-Type', /^application\/json/)
+                .expect(200)
+                .then(function (res) {
+                    expect(skuApiService.getSkusById).to.have.been.called;
                     var checkSku = res.body;
                     expect(checkSku).to.have.property('name').that.equals(input.name);
                 });
@@ -163,13 +168,16 @@ describe('Http.Api.Skus.2.0', function() {
         it('should 200 reputting the same sku', function() {
             skuApiService.upsertSku.resolves(input);
             return helper.request().put('/api/2.0/skus')
-            .send(input)
-            .expect(200);
+                .send(input)
+                .expect(200)
+                .then(function (res) {
+                    expect(skuApiService.upsertSku).to.have.been.called;
+                });
         });
 
         describe('PATCH /skus/:id', function () {
             var updated;
-            var updated_input = {
+            var updatedInput = {
                 name: 'updated sku name',
                 rules: [
                     {
@@ -192,19 +200,20 @@ describe('Http.Api.Skus.2.0', function() {
 
             it('should Patch updated name to the sku', function () {
                 sku.name = 'updated sku name';
-                skuApiService.patchSku.resolves(updated_input);
+                skuApiService.patchSku.resolves(updatedInput);
                 return helper.request().patch('/api/2.0/skus/0987')
                     .send(input)
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .then(function (res) {
+                        expect(skuApiService.patchSku).to.have.been.called;
                         updated = res.body;
                         expect(updated).to.have.property('name').that.equals(sku.name);
                         expect(updated).to.have.property('rules').that.deep.equals(sku.rules);
                     });
             });
-
         });
+
         describe('GET /skus/:id/nodes', function () {
             it('should contain the node', function () {
                 skuApiService.getNodesSkusById.resolves([node]);
@@ -212,6 +221,7 @@ describe('Http.Api.Skus.2.0', function() {
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function (res) {
+                        expect(skuApiService.getNodesSkusById).to.have.been.called;
                         expect(res.body).to.be.an.instanceof(Array);
                         expect(res.body).to.have.length(1);
                         expect(res.body[0]).to.have.property('id').that.equals(node.id);
@@ -220,28 +230,36 @@ describe('Http.Api.Skus.2.0', function() {
         });
 
         describe('DELETE /skus/:id', function () {
-
             beforeEach('DELETE /skus/:id', function () {
                 skuApiService.deleteSkuById.resolves();
                 return helper.request().delete('/api/2.0/skus/' + sku.id)
                     .expect(200)
-            })
+                    .then(function () {
+                        expect(skuApiService.deleteSkuById).to.have.been.called;
+                    });
+            });
 
             it('should 404 with GET /skus/:id ', function () {
                 skuApiService.getSkusById.rejects(new Errors.NotFoundError('Not Found'));
                 return helper.request().get('/api/2.0/skus/0987')
                     .expect(404)
+                    .then(function () {
+                        expect(skuApiService.getSkusById).to.have.been.called;
+                    });
             });
 
             it('should return an empty array from GET /skus', function () {
                 skuApiService.getSkus.resolves([]);
                 return helper.request().get('/api/2.0/skus')
-                .expect('Content-Type', /^application\/json/)
-                .expect(200, []);
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200, [])
+                    .then(function () {
+                        expect(skuApiService.getSkus).to.have.been.called;
+                    });
             });
         });
 
-        describe('PUT /skus/pack', function () {
+        describe('Process file for sku pack', function () {
             var skuService;
 
             beforeEach('setup', function () {
@@ -278,6 +296,64 @@ describe('Http.Api.Skus.2.0', function() {
             });
         });
 
+        describe('POST sku pack', function () {
+            beforeEach('setup', function () {
+                sinon.stub(skuApiService, 'skuPackHandler');
+                skuApiService.skuPackHandler.resolves();
+            });
+            afterEach('teardown', function () {
+                skuApiService.skuPackHandler.restore();
+            });
+            it('should post a sku pack', function (done) {
+                var fs = helper.injector.get('fs');
+                var skuPack = {id: 'sku name test'};
+                skuApiService.skuPackHandler.resolves({
+                    id: "sku name test"
+                });
+                var tarz = fs.createReadStream('spec/lib/services/sku-static/pack.tar.gz');
+                tarz.on('open', function() {
+                    return helper.request().post('/api/2.0/skus/pack')
+                        .set('Content-Type',  'application/x-www-form-urlencoded')
+                        .send(tarz)
+                        .expect(201)
+                        .end(function (err,res) {
+                            expect(skuApiService.skuPackHandler).to.have.been.called;
+                            expect(res.body).to.have.property('id').that.equals(skuPack.id);
+                            done();
+                        });
+                });
+            });
+        });
+
+        describe('PUT sku pack', function () {
+            beforeEach('setup', function () {
+                sinon.stub(skuApiService, 'putPackBySkuId');
+                skuApiService.putPackBySkuId.resolves();
+            });
+            afterEach('teardown', function () {
+                skuApiService.putPackBySkuId.restore();
+            });
+            it('should put a sku pack', function (done) {
+                var fs = helper.injector.get('fs');
+                var skuPack = {id : 'sku name test'};
+                waterline.skus.needByIdentifier.resolves({id: 'abc', sku: 'sku'});
+                skuApiService.putPackBySkuId.resolves({
+                    id: "sku name test"
+                });
+                var tarz = fs.createReadStream('spec/lib/services/sku-static/pack.tar.gz');
+                tarz.on('open', function() {
+                    return helper.request().put('/api/2.0/skus/' + skuPack.id + '/pack')
+                        .set('Content-Type',  'application/x-www-form-urlencoded')
+                        .send(tarz)
+                        .expect(200)
+                        .end(function (err,res) {
+                            expect(skuApiService.putPackBySkuId).to.have.been.called;
+                            expect(res.body).to.have.property('id').that.equals(skuPack.id);
+                            done();
+                        });
+                });
+            });
+        });
     });
 });
 
