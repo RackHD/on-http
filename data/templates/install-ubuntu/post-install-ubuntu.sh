f6a44545b0558887efe9fea275da15a41e6a2b41
PengTian0 <Tian.Peng@emc.com>
2016-05-17 16:59:18 +0800

    change uid to optional

--- data/templates/install-ubuntu/post-install-ubuntu.sh
+++ data/templates/install-ubuntu/post-install-ubuntu.sh
@@ -9,7 +9,11 @@ chown -R root:root /root/.ssh
 
 # create users and SSH key for users
 <% users.forEach(function(user) { -%>
-useradd -u <%=user.uid%> -m -p <%=user.encryptedPassword%> <%=user.name%>
+    <%_ if (undefined !== user.uid) { _%>
+        useradd -u <%=user.uid%> -m -p <%=user.encryptedPassword%> <%=user.name%>
+    <%_ } else {_%>
+        useradd -m -p <%=user.encryptedPassword%> <%=user.name%>
+    <%_ } _%>
     <%_ if (undefined !== user.sshKey) { _%>
 mkdir /home/<%=user.name%>/.ssh
 echo <%=user.sshKey%> > /home/<%=user.name%>/.ssh/authorized_keys
