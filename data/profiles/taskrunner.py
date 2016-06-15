99372a9e19e8639aa5fd144502b68f6882e9b516
dpasupathi <pasu@brocade.com>
2016-06-15 15:02:25 -0700

    Brocade VDX switch integration as part of switch discovery

--- data/profiles/taskrunner.py
+++ data/profiles/taskrunner.py
@@ -6,10 +6,11 @@
 Download and execute python scripts from the RackHD tasks api in a loop
 """
 
-import urllib2
+import requests
 import json
 import sys
 from time import sleep
+
 
 TASK_REQUEST_PERIOD = 5
 TASKS_URI = 'http://<%=server%>:<%=port%>/api/common/tasks/<%=identifier%>'
@@ -20,7 +21,12 @@ def download_task_data():
     """
     Download a python script from the tasks API
     """
-    _task_data = urllib2.urlopen(TASKS_URI).read()
+    print("Download Task", TASKS_URI)
+    session = requests.Session()
+    response = requests.Response()
+    response = session.get(TASKS_URI, auth=("", ""))
+    _task_data = response.text
+    session.close()
     return json.loads(_task_data)
 
 
@@ -28,17 +34,21 @@ def download_script(downloadUrl):
     """
     Download a python script from the tasks API
     """
-    url = urllib2.urlparse.urljoin('http://<%=server%>:<%=port%>', downloadUrl)
-    print "Downloading script at {}".format(url)
-    script = urllib2.urlopen(url).read()
+    session = requests.Session()
+    script = requests.Response()
+    url = 'http://<%=server%>:<%=port%>' + downloadUrl
+    print("Downloading script at {}".format(url))
+    script = session.get(url, auth=("", ""))
+    session.close()
     with open('script.py', 'w') as rackhd_script:
-        rackhd_script.write(script)
+        rackhd_script.write(script.text)
 
 while True:
     try:
+        print("task")
         task_data = download_task_data()
     except Exception as e:
-        print "Failed to download task data, sleeping for {} seconds".format(TASK_REQUEST_PERIOD)
+        print("Failed to download task data, sleeping for {} seconds".format(TASK_REQUEST_PERIOD))
         sleep(TASK_REQUEST_PERIOD)
         continue
 
@@ -47,7 +57,7 @@ while True:
             download_script(task['downloadUrl'])
             script_locals = {}
 
-            execfile('./script.py', script_locals)
+            exec(open("./script.py").read(), script_locals)
             script_main = script_locals['main']
 
             # TODO: support task_data['result'] on the server-side since this isn't
@@ -57,26 +67,35 @@ while True:
             # stringified JSON so make it easy for the server
             task['stdout'] = json.dumps(result)
         except Exception as error:
-            print "Failure running task {}".format(error)
+            print("Failure running task {}".format(error))
             task['error'] = str(error)
             break
 
     try:
         if "exit" in task_data.keys():
-            print "Task execution complete"
+            print("Task execution complete")
             sys.exit(int(task_data["exit"]))
         task_data = json.dumps(task_data)
-        req = urllib2.Request(TASKS_URI, task_data, json_content_type)
+        session = requests.Session()
+        req = requests.Response()
+        req = session.post(TASKS_URI, data=task_data, auth=("", ""), headers=json_content_type)
+        session.close()
     except Exception as error:
         task_data = [{'error': str(error)}]
         task_data = json.dumps(task_data)
-        req = urllib2.Request(TASKS_URI, task_data, json_content_type)
+        session = requests.Session()
+        req = requests.Response()
+        req = session.post(TASKS_URI, data=task_data, auth=("", ""), headers=json_content_type)
+        session.close()
 
     for _ in range(3):
         try:
-            print "Posting task data\n {}".format(task_data)
-            urllib2.urlopen(req)
-        except urllib2.URLError:
+            print("Posting task data\n {}".format(task_data))
+            session = requests.Session()
+            response = requests.Response()
+            response = session.get(req)
+            session.close()
+        except:
             sleep(TASK_REQUEST_PERIOD)
             continue
         break
