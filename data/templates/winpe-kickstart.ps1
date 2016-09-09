bbfb7fd52fce29fcc5d2b0690997d56da6b60172
lanchongyizu <lanchongyizu@gmail.com>
2016-09-08 23:13:31 -0400

    * replace completionUri with notification in OS kickstart files * additional works include: 	- esx: move the install-os completion into esx-ks %post, and remove postLookup 	- windows: move the install-os completion into winpe-kickstart.ps1

--- data/templates/winpe-kickstart.ps1
+++ data/templates/winpe-kickstart.ps1
@@ -5,3 +5,4 @@ Start-Sleep -s 2
 net use w: ${repo} ${smb_passwd} /user:${smb_user}
 Start-Sleep -s 2
 w:\setup.exe /unattend:x:\Windows\System32\unattend.xml
+curl -Method POST -ContentType 'application/json' http://<%=server%>:<%=port%>/api/current/notification?nodeId=<%=nodeId%> -Outfile winpe-kickstart.log
