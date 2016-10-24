d9e8fffd33e101bdd542c7c28a7cbe5d7e32f631
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2016-10-24 16:57:25 -0700

    Return original swagger-validator error messages to client

--- spec/lib/fittings/json-error-handler-spec.js
+++ spec/lib/fittings/json-error-handler-spec.js
@@ -9,17 +9,36 @@ describe('Swagger error handler fitting', function() {
     var context;
     var errorHandler;
     var mockNext;
+    var swaggerService;
+
+    before('inject swagger service', function() {
+        helper.setupInjector(_.flattenDeep([
+                onHttpContext.prerequisiteInjectables,
+                onHttpContext.injectables,
+            ])
+        );
+        swaggerService = helper.injector.get('Http.Services.Swagger');
+        sinon.stub(swaggerService, 'renderer');
+    });
 
     beforeEach('setup serdes test', function() {
         var res = {
             status: sinon.stub(),
-            json: sinon.stub()
+            json: sinon.stub(),
+            locals: {
+                uuid: 'foo'
+            }
         };
         context = {
+            request: {},
             response: res
         };
-        errorHandler = fitting();
+        errorHandler = fitting(null, null, helper.injector);
         mockNext = sinon.stub();
+    });
+
+    afterEach(function() {
+        swaggerService.renderer.reset();
     });
 
     it('should parse an Error object', function() {
@@ -28,8 +47,15 @@ describe('Swagger error handler fitting', function() {
         context.error = err;
         errorHandler(context, mockNext);
 
-        expect(context.response.status).to.be.calledWith(400);
-        expect(context.response.json).to.be.calledWith({message: err.message});
+        expect(swaggerService.renderer).to.be.calledWith(
+            context.request,
+            context.response
+        );
+        expect(context.response.body).to.deep.equal({
+            message: 'error message',
+            status: 400,
+            uuid: 'foo'
+        });
         expect(mockNext).to.not.be.called;
     });
 
@@ -40,8 +66,16 @@ describe('Swagger error handler fitting', function() {
         context.error = err;
         errorHandler(context, mockNext);
 
-        expect(context.response.status).to.be.calledWith(404);
-        expect(context.response.json).to.be.calledWith({message: err.message});
+        expect(swaggerService.renderer).to.be.calledWith(
+            context.request,
+            context.response
+        );
+        expect(context.response.body).to.deep.equal({
+            message: 'error message',
+            status: 404,
+            uuid: 'foo'
+        });
+
         expect(mockNext).to.not.be.called;
     });
 
@@ -52,8 +86,65 @@ describe('Swagger error handler fitting', function() {
         context.error = err;
         errorHandler(context, mockNext);
 
-        expect(context.response.status).to.be.calledWith(404);
-        expect(context.response.json).to.be.calledWith({message: http.STATUS_CODES[404]});
+        expect(swaggerService.renderer).to.be.calledWith(
+            context.request,
+            context.response
+        );
+        expect(context.response.body).to.deep.equal({
+            message: http.STATUS_CODES[404],
+            status: 404,
+            uuid: 'foo'
+        });
+
+        expect(mockNext).to.not.be.called;
+    });
+
+    it('should parse an Error object with error messages', function() {
+        var err = new Error('error message');
+        err.status = 404;
+        // Error object from:
+        // https://github.com/theganyo/swagger-node-runner/blob/master/fittings/swagger_validator.js#L24
+        // and from:
+        // https://github.com/apigee-127/sway/blob/master/lib/types/operation.js#L236
+        err.errors = [
+            { code: 'INVALID_REQUEST_PARAMETER',
+              errors:
+               [ { code: 'OBJECT_MISSING_REQUIRED_PROPERTY',
+                   message: 'Missing required property: nodeId',
+                   path: [],
+                   description: 'OBM settings' } ],
+              in: 'body',
+              message: 'Invalid parameter (body): Value failed JSON Schema validation',
+              name: 'body',
+              path: [ 'paths', '/obms', 'put', 'parameters', '0' ] },
+            { code: 'INVALID_REQUEST_PARAMETER',
+              errors:
+               [ { code: 'OBJECT_MISSING_REQUIRED_PROPERTY',
+                   message: 'Missing required property: service',
+                   path: [],
+                   description: 'OBM settings' } ],
+              in: 'body',
+              message: 'Invalid parameter (body): Value failed JSON Schema validation',
+              name: 'body',
+              path: [ 'paths', '/obms', 'put', 'parameters', '0' ] }
+        ];
+
+        context.error = err;
+        errorHandler(context, mockNext);
+
+        expect(swaggerService.renderer).to.be.calledWith(
+            context.request,
+            context.response
+        );
+        expect(context.response.body).to.deep.equal({
+            message: 'error message',
+            status: 404,
+            uuid: 'foo',
+            errors: [
+                'Missing required property: nodeId',
+                'Missing required property: service'
+            ]
+        });
         expect(mockNext).to.not.be.called;
     });
 
@@ -63,8 +154,6 @@ describe('Swagger error handler fitting', function() {
         context.error = err;
         errorHandler(context, mockNext);
 
-        expect(context.response.status).to.not.be.called;
-        expect(context.response.json).to.not.be.called;
         expect(mockNext).to.be.called;
     });
 });
