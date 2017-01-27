cfdc41e06a99e099a7d8cd25dc618810c64b581d
James King <king.jam@husky.neu.edu>
2017-01-26 20:20:59 -0500

    Additional Details to Clarify Proposed Changes
    Fixed typos
    Further explained the issue and need for fix

--- data/templates/cisco-catalog-version.py
+++ data/templates/cisco-catalog-version.py
@@ -8,8 +8,11 @@ def main():
     data = {}
 
     try:
-        # The "as is" will cause dumps from taskrunner.py to produce an invalid jason string
+        # get the version in json form in a temporary string so we can manipulate it
         dataString = clid('show version')
+        # The "as is" in the license header string from 'show version'
+        # will cause json.dumps within taskrunner.py to produce an invalid json string
+        # This is resolved by replacing the additional escapes (\) with a raw string
         dataString = dataString.replace("\\\"as is,\\\"", "as is")
         data = json.loads(dataString)
     except:
