c66f56b2453571ac5fe72a68a621c0a1dd29c91e
Azzam Tannous <azzam.tannous@emc.com>
2017-06-21 14:11:55 +0000

    Add serial and part number to the redfish alert

--- spec/lib/services/notification-api-service-spec.js
+++ spec/lib/services/notification-api-service-spec.js
@@ -24,22 +24,46 @@ describe('Http.Api.Notification', function () {
 
     var bmc = {
         'IP Address': '10.240.19.130',
-        node : "abc",
-        data : {'MAC Address': "jill"}
+        node: "abc",
+        data: {'MAC Address': "jill"}
     };
-    var deviceInfoCatalog ={
-        data : {
-            'Builtin FRU Device (ID 0)' : {
-                'Board Product' : "bernie",
-                'Product Serial' : "hillary",
-                'Board Serial' : "theDonald"
+    var deviceInfoCatalog = {
+        data: {
+            'Builtin FRU Device (ID 0)': {
+                'Board Product': "bernie",
+                'Product Serial': "hillary",
+                'Board Serial': "theDonald"
+            }
+        }
+    };
+    var ooc =  "NonDisk.Direct.0-0:AHCI.Embedded.1-1";
+    var catalogFqdd = "Disk.Direct.0-0:AHCI.Embedded.1-1";
+
+    var wsmanCatalog =
+    {
+        "data": {
+            "storage": {
+                "physicalDisks": [
+                    {
+                        "fqdd": catalogFqdd,
+                        "mediaType": "0",
+                        "serialNumber": "9XG9PVW3",
+                        "instanceID": null,
+                        "sasaddress": "5000C50087FA4387",
+                        "deviceDescription": "Disk 0 on Embedded AHCI Controller 1",
+                        "manufacturer": "SEAGATE",
+                        "model": "ST91000640NS"
+                    }
+                ]
             }
         }
     };
     var alert = {
-        "Severity":"Critical"
+        "Severity": "Critical",
+        "OriginOfCondition": ooc
     };
-    var amqpMessage ={action: "alerts",
+    var amqpMessage = {
+        action: "alerts",
         data: {
             ChassisName: "bernie",
             SN: "theDonald",
@@ -47,12 +71,16 @@ describe('Http.Api.Notification', function () {
             Severity: "Critical",
             sourceMacAddress: "jill",
             nodeId: "abc",
-            sourceIpAddress: "127.0.0.1"
+            sourceIpAddress: "127.0.0.1",
+            OriginOfCondition: "NonDisk.Direct.0-0:AHCI.Embedded.1-1",
+            originOfConditionPartNumber: null,
+            originOfConditionSerialNumber: null
         },
         severity: "critical",
         type: "node",
         typeId: "abc"
     };
+
     var req = {
         headers : {
             'x-forwarded-for' :"127.0.0.1"
@@ -211,6 +239,7 @@ describe('Http.Api.Notification', function () {
             waterline.obms.findOne.onCall(1).resolves(undefined);
             waterline.catalogs.findMostRecent.onCall(0).resolves(bmc);
             waterline.catalogs.findMostRecent.onCall(1).resolves(deviceInfoCatalog);
+            waterline.catalogs.findMostRecent.onCall(2).resolves(wsmanCatalog);
             return notificationApiService.redfishAlertProcessing(req)
                 .then(function (resp) {
                     expect(resp).to.deep.equal(amqpMessage);
@@ -222,6 +251,8 @@ describe('Http.Api.Notification', function () {
             waterline.obms.findOne.onCall(0).resolves({config : {host: "0.0.0.0"}});
             waterline.catalogs.findMostRecent.onCall(0).resolves(bmc);
             waterline.catalogs.findMostRecent.onCall(1).resolves(deviceInfoCatalog);
+            wsmanCatalog.data.storage.fqdd=  catalogFqdd;
+            waterline.catalogs.findMostRecent.onCall(2).resolves(wsmanCatalog);
             return notificationApiService.redfishAlertProcessing(req)
                 .then(function (resp) {
                     expect(resp).to.deep.equal(amqpMessage);
@@ -233,12 +264,17 @@ describe('Http.Api.Notification', function () {
             waterline.obms.findOne.onCall(0).resolves({config : {host: "00:00:00:00:00"}});
             waterline.catalogs.findMostRecent.onCall(0).resolves(bmc);
             waterline.catalogs.findMostRecent.onCall(1).resolves(deviceInfoCatalog);
+            ooc =  "NonDisk.Direct.0-0:AHCI.Embedded.1-1";
+            catalogFqdd = "Disk.Direct.0-0:AHCI.Embedded.1-1";
+
+            amqpMessage.data.OriginOfCondition = ooc;
+            wsmanCatalog.data.storage.fqdd =  catalogFqdd;
+            waterline.catalogs.findMostRecent.onCall(2).resolves(wsmanCatalog);
             return notificationApiService.redfishAlertProcessing(req)
                 .then(function (resp) {
                     expect(resp).to.deep.equal(amqpMessage);
                 });
         });
-
 
         it('should NOT post an alert notification for an unrecognized node', function (done) {
             this.sandbox.restore();
@@ -271,6 +307,66 @@ describe('Http.Api.Notification', function () {
                 .catch(function (e) {
                     expect(e).to.have.property('message').that.equals('Unexpected alert data');// jshint ignore:line
                     done();
+                });
+        });
+
+        it('should post an alert notification successfully(different catalog fbdd and alert originOfCondition)', function () {// jshint ignore:line
+            //originOfCondition:"Disk.Bay.1-7" is equivalent to FQDD: "Disk.Direct.3-7:AHCI.Embedded.2-1"
+            this.sandbox.restore();
+            ooc =  "Disk.Bay.1-7";
+            catalogFqdd = "Disk.Direct.3-7:AHCI.Embedded.2-1";
+            alert = {
+                "Severity": "Critical",
+                "OriginOfCondition": ooc
+            };
+            req = {
+                headers : {
+                    'x-forwarded-for' :"127.0.0.1"
+                },
+                swagger: { query: alert}
+            };
+            wsmanCatalog.data.storage.physicalDisks[0].fqdd = catalogFqdd;
+            waterline.obms.findOne.onCall(0).resolves({config : {host: "00:00:00:00:00"}});
+            waterline.catalogs.findMostRecent.onCall(0).resolves(bmc);
+            waterline.catalogs.findMostRecent.onCall(1).resolves(deviceInfoCatalog);
+            amqpMessage.data.OriginOfCondition = ooc;
+            amqpMessage.data.originOfConditionPartNumber = wsmanCatalog.data.storage.physicalDisks[0].model;
+            amqpMessage.data.originOfConditionSerialNumber = wsmanCatalog.data.storage.physicalDisks[0].serialNumber;
+            wsmanCatalog.data.storage.fqdd =  catalogFqdd;
+            waterline.catalogs.findMostRecent.onCall(2).resolves(wsmanCatalog);
+            return notificationApiService.redfishAlertProcessing(req)
+                .then(function (resp) {
+                    expect(resp).to.deep.equal(amqpMessage);
+                });
+        });
+
+        it('should post an alert notification successfully("couldn NOT find catalog info")', function () {// jshint ignore:line
+
+            this.sandbox.restore();
+            ooc =  "Disk.Bay.11";
+            catalogFqdd = "Disk.Direct.3-7:AHCI.Embedded.2-1";
+            alert = {
+                "Severity": "Critical",
+                "OriginOfCondition": ooc
+            };
+            req = {
+                headers : {
+                    'x-forwarded-for' :"127.0.0.1"
+                },
+                swagger: { query: alert}
+            };
+            wsmanCatalog.data.storage.physicalDisks.fqdd = catalogFqdd;
+            waterline.obms.findOne.onCall(0).resolves({config : {host: "00:00:00:00:00"}});
+            waterline.catalogs.findMostRecent.onCall(0).resolves(bmc);
+            waterline.catalogs.findMostRecent.onCall(1).resolves(deviceInfoCatalog);
+            amqpMessage.data.OriginOfCondition = ooc;
+            amqpMessage.data.originOfConditionPartNumber = null;
+            amqpMessage.data.originOfConditionSerialNumber = null;
+            wsmanCatalog.data.storage.fqdd =  catalogFqdd;
+            waterline.catalogs.findMostRecent.onCall(2).resolves(wsmanCatalog);
+            return notificationApiService.redfishAlertProcessing(req)
+                .then(function (resp) {
+                    expect(resp).to.deep.equal(amqpMessage);
                 });
         });
     });
