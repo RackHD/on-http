b6269b3119c50fa9d271e1532c0850a32cb0b757
mcgg <zhaokaihao@gmail.com>
2017-11-20 05:15:29 -0500

    Add retry in swagger and web ui script

--- install-swagger-ui.sh
+++ install-swagger-ui.sh
@@ -2,7 +2,12 @@
 
 cd ./static
 rm -rf swagger-ui
-curl -L -o swagger-ui.zip https://github.com/rackhd/swagger-ui/archive/master.zip
+curl --fail -L -o swagger-ui.zip https://github.com/rackhd/swagger-ui/archive/master.zip
+if [ $? -ne 0 ]; then
+    echo 'failed to download, retrying'
+    curl --fail -L -o swagger-ui.zip https://github.com/rackhd/swagger-ui/archive/master.zip \
+        || { echo "failed to download, exiting" && exit 1; }
+fi
 unzip swagger-ui.zip swagger-ui-master/dist* -d swagger-ui
 mv swagger-ui/swagger-ui-master/dist/* swagger-ui
 rm swagger-ui.zip
