514770720062c7f6fe8cdd8a208f0cfe38e548cf
Linglin.Yao <Lingling.Yao@emc.com>
2018-06-13 01:55:52 -0400

    refine installation script for web-ui-2.0.

--- install-web-ui.sh
+++ install-web-ui.sh
@@ -1,16 +1,22 @@
 # Copyright 2015-2017, Dell EMC, Inc.
 
 cd ./static
-
 rm -rf web-ui
-curl --fail -L -o on-web-ui-gh-pages.zip https://github.com/RackHD/on-web-ui/archive/gh-pages.zip
+version=$(curl https://raw.githubusercontent.com/rackhd/rackhd-ui-2.0/version/version)
+curl --fail -L -o on-web-ui-gh-pages.zip https://github.com/rackhd/rackhd-ui-2.0/archive/${version}.zip
 
 if [ $? -ne 0 ]; then
     echo 'failed to download, retrying'
-    curl --fail -L -o on-web-ui-gh-pages.zip https://github.com/RackHD/on-web-ui/archive/gh-pages.zip \
+    curl --fail -L -o on-web-ui-gh-pages.zip https://github.com/rackhd/rackhd-ui-2.0/archive/${version}.zip \
         || { echo "failed to download, exiting" && exit 1; }
 fi
 
 unzip on-web-ui-gh-pages.zip
 rm on-web-ui-gh-pages.zip
-mv on-web-ui-gh-pages web-ui
+mv rackhd-ui-2.0-${version} web-ui
+
+pushd web-ui
+npm install webpack-dev-server rimraf webpack -g
+npm install
+npm run build:aot
+popd
